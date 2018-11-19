73c00fc8980119222525be243a12182f48cbf4d7
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-19 18:18:17 +0000

    typings: add typings

---
+++ typings/main.d.ts
@@ -0,0 +1,8 @@
+﻿declare class BWAPIWrapper {
+    private BASE_URL;
+    private HEADERS;
+    constructor(headers?: object);
+    request(path: string, qs?: object): Promise<any>;
+}
+export = BWAPIWrapper;
+//# sourceMappingURL=main.d.ts.map
