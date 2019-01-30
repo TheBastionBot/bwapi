11b6c73dca43235f0d59afb2043d604d35a24174
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-01-30 18:15:58 +0000

    typings: update

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,9 +1,9 @@
-﻿import * as request from "request-promise-native";
+﻿import * as Request from "request-promise-native";
 declare class BWAPIWrapper {
     private BASE_URL;
     private OPTIONS;
-    constructor(options?: request.RequestPromiseOptions);
-    request(path: string, options?: request.RequestPromiseOptions): Promise<any>;
+    constructor(options?: Request.RequestPromiseOptions);
+    request(path: string, options?: Request.RequestPromiseOptions): Promise<any>;
 }
 export = BWAPIWrapper;
 //# sourceMappingURL=main.d.ts.map
