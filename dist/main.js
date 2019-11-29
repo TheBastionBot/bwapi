15a5d3e806f3d22d517c3a59201bd4c1aeb61739
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-11-29 16:12:57 +0530

    dist: update transpiled JS

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,17 @@
 ﻿"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
 const Request = require("request-promise-native");
 class BWAPIWrapper {
     constructor(options) {
-        this.BASE_URL = "https://api.bastionbot.org";
+        this.BASE_URL = "https://thebastionbot.herokuapp.com";
         this.OPTIONS = {
             json: true,
         };
